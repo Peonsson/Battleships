14f86d3609a291bc2df02836570f436dad69c90b
Johan Pettersson <johanpettersson@n132-p213.kthopen.kth.se>
2015-09-08 15:17:42 +0200

    Initial commit

---
+++ src/Battleships.java
@@ -0,0 +1,18 @@
+/**
+ * Created by johanpettersson on 08/09/15.
+ */
+public class Battleships {
+
+    private int[][] player1;
+    private int[][] player2;
+
+    public Battleships() {
+        player1 = new int[10][10];
+        player2 = new int[10][10];
+    }
+
+    public int putShip(String orientation, int x, int y, int length) {
+
+        return 0;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by johanpettersson on 08/09/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Battleships myGame = new Battleships();
+    }
+}
